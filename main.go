c97fb817ca5720af101d73e3673bad64b421023d
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2021-08-20 15:42:40 -0700

    baseline convert media to rtp pkts

--- main.go
+++ main.go
@@ -2,20 +2,75 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
-func main() {
+var gstformat = `/usr/bin/gst-launch-1.0 filesrc location="%s" !
+decodebin name=decode !
+videoconvert !
+x264enc option-string=slice-max-size=1200 speed-preset=medium tune=zerolatency key-int-max=1 !
+video/x-h264,profile=constrained-baseline !
+queue max-size-time=100000000 !
+rtph264pay config-interval=-1 name=payloader !
+multifilesink location="%s"`
 
-	log.Printf("Running command and waiting for it to finish...")
+const infile = "/foo/idle-media"
+const outdir = "/foo/out.nosync"
 
-	cmd := exec.Command("/usr/bin/gst-inspect-1.0")
-	stdoutStderr, err := cmd.CombinedOutput()
+var gstcmd = fmt.Sprintf(gstformat, infile, outdir+"/rtp%d.rtp")
+
+func checkFatal(err error) {
 	if err != nil {
-		log.Fatal(err)
+		_, fileName, fileLine, _ := runtime.Caller(1)
+		log.Fatalf("FATAL %s:%d %v", filepath.Base(fileName), fileLine, err)
 	}
-	fmt.Printf("%s\n", stdoutStderr)
+}
 
-	log.Printf("Command finished with error: %v", err)
+func Exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		checkFatal(err)
+	}
+	return true
+}
+
+func main() {
+	var err error
+
+	if !Exists(infile) {
+		checkFatal(fmt.Errorf("no in file"))
+		// http server
+	}
+
+	_ = os.RemoveAll(outdir)
+	err = os.Mkdir(outdir, 0777)
+	checkFatal(err)
+
+	args := strings.Fields(gstcmd)
+
+	// for k, v := range args {
+	// 	println(k, v)
+	// }
+	log.Printf("Running command2")
+
+	cmd := exec.Command(args[0], args[1:]...)
+	stdoutStderr, err := cmd.CombinedOutput()
+	checkFatal(err)
+
+	fmt.Printf("output: %s\n", stdoutStderr)
+
+	log.Printf("Command err code: %v", err)
+
+	n, err := ioutil.ReadDir(outdir)
+	checkFatal(err)
+
+	log.Printf("n files after gstreamer: %v", len(n))
 }
